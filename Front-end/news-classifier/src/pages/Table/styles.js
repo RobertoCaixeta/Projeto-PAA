b506b2b8813ac9e478795b723a45782c754b8bdc
robertocaixeta <robertocaixeta102@gmail.com>
2022-08-28 19:17:35 -0300

    :sparkles: Adiciona o componente de table ao projeto

---
+++ Front-end/news-classifier/src/pages/Table/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  display: flex;
+  width: 50vw;
+  min-height: 100vh;
+  justify-content: center;
+  flex-direction: column; 
+  padding: 1%;
+  margin: 0 auto 0 auto;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  border-style:solid;
+  border-color: black;
+  border-radius: 20px;
+  justify-content: center;
+  flex-direction: column; 
+  padding: 1em;
+  margin-bottom: 1em;
+`
+export const Title = styled.h1`
+  font-weight: bold;
+`
+
+export const Text = styled.p`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 5;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  font-size: 18px;
+`
+export const Button = styled.button`
+  color: #ff4d4d;
+  font-size: 1em;
+  margin: 1em 0;
+  padding: 0.5em 1em;
+  border: 2px solid #ff4d4d;
+  border-radius: 10px;
+  width: 10em;
+`;
--- Front-end/news-classifier/src/pages/news/index.js
+++ Front-end/news-classifier/src/pages/news/index.js
@@ -1,7 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
+import Table from '../Table';
+
 
 export default function News() {
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+        api.get(`/news`).then(({ data }) => {
+            setNews(data)
+        })
+    }, []);
     return (
-        <div>salve</div>
+        <>
+        <Table props={news}/>
+        </>
     )
 }
