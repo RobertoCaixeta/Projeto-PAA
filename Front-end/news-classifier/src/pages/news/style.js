16e1ce9fc14065b2f77771afc93fa8ca282d84e2
robertocaixeta <robertocaixeta102@gmail.com>
2022-08-28 17:33:42 -0300

    :star: Cria componentes base par ao projeto

---
+++ Front-end/news-classifier/src/pages/news/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  display: flex;
+  min-width: 100vw;
+  min-height: 100vh;
+  justify-content: center;
+  background-color: #6c7775; 
+`;
