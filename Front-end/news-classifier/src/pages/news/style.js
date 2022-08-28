67bec4922f4dbd2e8b9670c0eafdac923a27bf59
robertocaixeta <robertocaixeta102@gmail.com>
2022-08-28 19:16:59 -0300

    :memo: Troca o nome dos arquivos para seguir o padrão

--- Front-end/news-classifier/src/pages/news/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Background = styled.div`
-  display: flex;
-  min-width: 100vw;
-  min-height: 100vh;
-  justify-content: center;
-  background-color: #6c7775; 
-`;
