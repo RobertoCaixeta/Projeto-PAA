1a41ae9fabdf89a5e52ee14b245315ac3347146a
robertocaixeta <robertocaixeta102@gmail.com>
2022-08-30 17:52:19 -0300

    :star: Adiciona navbar à página de News

--- Front-end/news-classifier/src/pages/news/index.js
+++ Front-end/news-classifier/src/pages/news/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../services/api'
-import Table from '../Table';
-
+import Table from '../../components/Table';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export default function News() {
     const [news, setNews] = useState([]);
@@ -13,7 +13,17 @@ export default function News() {
     }, []);
     return (
         <>
-        <Table props={news}/>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">News</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/liked">Liked</Nav.Link>
+                        <Nav.Link href="/disliked">Disliked</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <br />
+            <Table data={news} visible={true} />
         </>
     )
 }
