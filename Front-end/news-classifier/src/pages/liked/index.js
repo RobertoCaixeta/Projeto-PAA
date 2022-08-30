1bde2ce3a489a374a37ce7bda803a56a72facbbc
robertocaixeta <robertocaixeta102@gmail.com>
2022-08-30 17:51:57 -0300

    :sparkles: Implementa component de liked

---
+++ Front-end/news-classifier/src/pages/liked/index.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
+import Table from '../../components/Table';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+export default function Liked() {
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+        api.get(`/news`).then(({ data }) => {
+            setNews(data)
+        })
+    }, []);
+    return (
+        <>
+            <Navbar bg="danger" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#liked">Liked</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">News</Nav.Link>
+                        <Nav.Link href="/disliked">Disliked</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <br />
+            <Table data={news} visible={false} />
+        </>
+    )
+}
