27c926cc2264972a3612193d8cfc3db5e753b857
robertocaixeta <robertocaixeta102@gmail.com>
2022-09-16 20:03:55 -0300

    :sparkles: Implementa página de tags

--- Front-end/news-classifier/src/pages/tags/index.js
+++ Front-end/news-classifier/src/pages/tags/index.js
@@ -1,16 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../services/api'
-import Table from '../../components/Table';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Background } from './styles';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 
 export default function Tags() {
-    const [news, setNews] = useState([]);
+    const [tags, setTags] = useState([]);
+
+    const [tagsSelecionadas, setSelecionarTag] = useState([])
+
 
     useEffect(() => {
-        api.get(`/news`).then(({ data }) => {
-            setNews(data)
+        api.get(`/tags`).then(({ data }) => {
+            setTags(data)
         })
-    }, [news]);
+        api.get(`/tagsSelecionadas`).then(({ data }) => {
+            setSelecionarTag(data)
+        })
+    }, []);
+
+
+    function handleRemoveTag(tag) {
+        setTags(prevTags => [...prevTags, tag])
+        const newTags = tagsSelecionadas.filter((tags) => tags.id !== tag.id);
+        setSelecionarTag(newTags);
+    }
+
+    function selecionarTag(tag) {
+        setSelecionarTag(prevTags => [...prevTags, tag])
+        const newTags = tags.filter((tags) => tags.id !== tag.id);
+        setTags(newTags);
+    }
+
+    function selecionarTags() {
+
+        api.post(`/tagsSelecionadas`, tagsSelecionadas).then(({ data }) => {
+
+        })
+        api.post(`/tags`, tags).then(({ data }) => {
+
+        })
+        
+
+    }
+
+
     return (
         <>
             <Navbar bg="primary" variant="dark">
@@ -23,7 +58,26 @@ export default function Tags() {
                 </Container>
             </Navbar>
             <br />
-            <Table data={news} visible={false} color={"#0d6efd"} />
+            <Background>
+                <div>
+                    <h1>Tags Disponíveis</h1>
+                    {tags.map(tag => (
+                        <h1 style={{ display: "inline", margin: "10px" }} key={tag.id}>
+                            <Badge onClick={() => selecionarTag(tag)} bg="primary">{tag.Text}</Badge>
+                        </h1>
+                    ))}
+
+
+                    <h1 style={{ marginTop: "100px" }}>Tags Selecionadas</h1>
+                    {tagsSelecionadas.map(tag => (
+                        <h1 style={{ display: "inline", margin: "10px" }} key={tag.id}>
+                            <Badge onClick={() => handleRemoveTag(tag)} bg="danger">{tag.Text}</Badge>
+                        </h1>
+                    ))}
+                </div>
+                <Button variant="dark" style={{ marginTop: "100px" }} onClick={selecionarTags}>Selecionar Tags</Button>
+
+            </Background>
         </>
     )
 }
--- Front-end/news-classifier/src/pages/tags/styles.js
+++ Front-end/news-classifier/src/pages/tags/styles.js
@@ -1,9 +1,18 @@
 import styled from "styled-components";
 
-export const Background = styled.div`
+/*export const Background = styled.div`
   display: flex;
   min-width: 100vw;
   min-height: 100vh;
   justify-content: center;
   background-color: #6c7775; 
+`;*/
+export const Background = styled.div`
+  display: flex;
+  width: 50vw;
+  min-height: 100vh;
+  align-items: flex-start;
+  flex-direction: column; 
+  padding: 1%;
+  margin: 0 auto 0 auto;
 `;
