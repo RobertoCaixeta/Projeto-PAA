675d0272b46c4c3d2cea7280409e0f15c703498d
robertocaixeta <robertocaixeta102@gmail.com>
2022-09-16 18:00:21 -0300

    :sparkles: Adiciona funcionalidade de like na notícia

--- Front-end/news-classifier/src/components/Table/index.js
+++ Front-end/news-classifier/src/components/Table/index.js
@@ -1,25 +1,42 @@
 import React from 'react'
+import api from '../../services/api'
 import { Background, Container, Title, Text, Button } from './styles';
-import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai";
+import { AiOutlineLike } from "react-icons/ai";
 import { BsHeart } from "react-icons/bs";
+import Badge from 'react-bootstrap/Badge';
 
 export default function Table({ data, visible, color, liked }) {
+    function like(noticia) {
+        api.post(`/liked`, noticia).then(({ data }) => {
+        }).catch((error) => {
+            alert("Erro ao dar like na notícia!!")
+            console.log(error)
+        })
+        api.delete(`/news/${noticia.id}`).then(({ data }) => {
+            window.location.reload();
+        })
+    }
     return (
         <>
             <div>
                 <Background>
                     {data.map(noticia => (
-                        <Container key={data.id} color={color}>
+                        <Container key={noticia.id} color={color}>
                             <Title>{noticia.title}  </Title>
                             {visible &&
                                 <div>
-                                    <AiOutlineLike size={"2em"} />
-                                    <AiOutlineDislike size={"2em"} />
+                                    <AiOutlineLike size={"2em"} onClick={event => like(noticia)} />
+                                    {noticia.tags.map(tag => (
+                                        <Badge key={tag.id} bg="secondary" style={{ marginLeft: "4px" }}>{tag.Text}</Badge>
+                                    ))}
                                 </div>
                             }
                             {liked &&
                                 <div>
-                                    <BsHeart size={"2em"} color={color}/>
+                                    <BsHeart size={"2em"} color={color} />
+                                    {noticia.tags.map(tag => (
+                                        <Badge key={tag.id} bg="danger" style={{ marginLeft: "4px" }}>{tag.Text}</Badge>
+                                    ))}
                                 </div>
                             }
 
