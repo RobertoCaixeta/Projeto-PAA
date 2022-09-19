7096e2d8306a6bb2dc76b6f7682bec3c0ecb8900
Álvaro Luz <54986553+AlvaroLuz@users.noreply.github.com>
2022-09-18 22:31:18 -0300

    Update main.py
    versão final da main

--- Algoritmo/main.py
+++ Algoritmo/main.py
@@ -3,111 +3,119 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import nltk
-from random import randrange
+#from random import randrange
 import json
-nltk.download("stopwords")
+import threading
+ultimo = 0
+atual = 0
+def rank_alg():
+    nltk.download("stopwords")
+    dataset = json.loads(open("ranked.json","r").read())
+    
+    newsaux = dataset["news"]
+    likedaux = dataset["liked"]
+    
+    news = pd.DataFrame(columns=["id","tag", "title", "text","link"])
+    likes = pd.DataFrame(columns=["id","tag", "title", "text","link"])
+    
+    for new in newsaux:
+      aux = pd.DataFrame({"id":[new["id"]],"tag":[new["tag"]], "title":[new["title"]], "text":[new["text"]],"link":[new["link"]]})
+      news = pd.concat([news, aux],ignore_index = True , axis = 0)
+      
+      
+    for new in likedaux:
+      aux = pd.DataFrame({"id":[new["id"]],"tag":[new["tag"]], "title":[new["title"]], "text":[new["text"]],"link":[new["link"]]})
+      likes = pd.concat([likes, aux],ignore_index = True , axis = 0)
+    print(likes)
+    
+    #le o json
+    #likes = pd.read_json("liked.json", orient="table")
+    liked_tags = {}
+    
+    #conta pra ver qual tag foi mais curtida
+    soma = 0
+    for idx, row in likes.iterrows():
+        soma = soma + 1
+        if row["tag"] in liked_tags:
+            liked_tags[row["tag"]] = liked_tags[row["tag"]] + 1
+        else:
+            liked_tags[row["tag"]] = 1
+    
+    #print(liked_tags)
+    #gera um float de peso pra tag
+    for key in liked_tags:
+        liked_tags[key] = liked_tags[key] / soma
+    
+    
+    def semelhanca(titulo, tag, vectorizer,df):
+        #df = pd.read_json("liked.json", orient="table")
+        df = df.loc[df["tag"] == tag]
+        if df.empty:
+            return 0.1
+        else:
+            avg = []
+            for _, row in df.iterrows():
+                vect = vectorizer.fit_transform([titulo, row["title"]])
+                avg.append(cosine_similarity(vect[0:1], vect)[0][1])
+            return sum(avg) / len(avg)
+    
+    
+    #calcula a pontuacao da noticia
+    rmd = news
+    rmd = rmd.loc[:, ~rmd.columns.str.contains('^Unnamed')]
+    stopwords = nltk.corpus.stopwords.words("portuguese")
+    vectorizer = TfidfVectorizer(stop_words=stopwords)
+    score = np.zeros(len(rmd.index))
+    for idx, row in rmd.iterrows():
+        if liked_tags.get(row["tag"]) != None:
+            score[idx] = liked_tags[row["tag"]]
+        else:
+            score[idx] = 1 / soma
+        score[idx] = 20 * score[idx] + semelhanca(row["title"], row["tag"], vectorizer,likes)
+    
+    #print (score)
+    
+    rmd = rmd.assign(score=score)
+    rmd = rmd.sort_values(by="score", ascending=False)
+    #print(rmd)
+    #rmd.to_json("ranked.json", orient="table")
+    
+    news = []
+    liked = []
+    for idx, row in rmd.iterrows():
+        noticia = {}
+        noticia["tag"] = row["tag"]
+        noticia["id"] = idx
+        noticia["title"] = row["title"]
+        noticia["text"] = row["text"] 
+        noticia["link"] = row["link"]
+        news.append(noticia)
+    
+    for idx, row in likes.iterrows():
+        noticia = {}
+        noticia["tag"] = row["tag"]
+        noticia["id"] = idx
+        noticia["title"] = row["title"]
+        noticia["text"] = row["text"] 
+        noticia["link"] = row["link"]
+        liked.append(noticia)
+    
+    aux = {"news":news, "liked":liked}
+    with open("ranked.json", "w") as f:
+    	  json.dump (aux, f)
 
-news = pd.read_xml("filtrado.xml")
+def foo():
+    global ultimo
+    global atual
+    liked = json.loads(open("ranked.json","r").read())
+    atual = len(liked['liked'])
+    if(atual != ultimo):
+        rank_alg()
+        ultimo = atual
+    threading.Timer(10, foo).start()
 
-tags = []
-for idx, new in news.iterrows():
-    if new["Tag"] not in tags:
-        tags.append(new["Tag"])
+def main():
+    foo()
 
-#lista com todas as tag
-taglist = tags
-
-#separando entre noticias com like aleatoriamente
-likes = pd.DataFrame(columns=news.columns)
-no_likes = pd.DataFrame(columns=news.columns)
-
-for idx, new in news.iterrows():
-    chance = randrange(2)
-    if chance == 0:
-        aux = pd.DataFrame([new])
-        likes = pd.concat([likes, aux], axis=0, ignore_index=True)
-    else:
-        aux = pd.DataFrame([new])
-        no_likes = pd.concat([no_likes, aux], axis=0, ignore_index=True)
-
-#print(likes)
-#joga tudo num json separado por tab
-likes.to_json("liked.json", orient="table")
-no_likes.to_json("not_liked.json", orient="table")
-
-
-#le o json
-likes = pd.read_json("liked.json", orient="table")
-liked_tags = {}
-
-#conta pra ver qual tag foi mais curtida
-soma = 0
-for idx, row in likes.iterrows():
-    soma = soma + 1
-    if row["Tag"] in liked_tags:
-        liked_tags[row["Tag"]] = liked_tags[row["Tag"]] + 1
-    else:
-        liked_tags[row["Tag"]] = 1
-
-#print(liked_tags)
-#gera um float de peso pra tag
-for key in liked_tags:
-    liked_tags[key] = liked_tags[key] / soma
-
-
-def semelhanca(titulo, tag, vectorizer):
-    df = pd.read_json("liked.json", orient="table")
-    df = df.loc[df["Tag"] == tag]
-    if df.empty:
-        return 0.1
-    else:
-        avg = []
-        for _, row in df.iterrows():
-            vect = vectorizer.fit_transform([titulo, row["Titulo"]])
-            avg.append(cosine_similarity(vect[0:1], vect)[0][1])
-        return sum(avg) / len(avg)
-
-
-#calcula a pontuacao da noticia
-rmd = pd.read_json("not_liked.json", orient="table")
-rmd = rmd.loc[:, ~rmd.columns.str.contains('^Unnamed')]
-stopwords = nltk.corpus.stopwords.words("portuguese")
-vectorizer = TfidfVectorizer(stop_words=stopwords)
-score = np.zeros(len(rmd.index))
-for idx, row in rmd.iterrows():
-    if liked_tags.get(row["Tag"]) != None:
-        score[idx] = liked_tags[row["Tag"]]
-    else:
-        score[idx] = 1 / soma
-    score[idx] = 20 * score[idx] + semelhanca(row["Titulo"], row["Tag"], vectorizer)
-
-#print (score)
-
-rmd = rmd.assign(score=score)
-rmd = rmd.sort_values(by="score", ascending=False)
-#print(rmd)
-#rmd.to_json("ranked.json", orient="table")
-
-news = []
-liked = []
-for idx, row in rmd.iterrows():
-    noticia = {}
-    noticia["tag"] = row["Tag"]
-    noticia["id"] = idx
-    noticia["title"] = row["Titulo"]
-    noticia["text"] = row["Subtitulo"] 
-    noticia["link"] = row["Link"]
-    news.append(noticia)
-
-for idx, row in likes.iterrows():
-    noticia = {}
-    noticia["tag"] = row["Tag"]
-    noticia["id"] = idx
-    noticia["title"] = row["Titulo"]
-    noticia["text"] = row["Subtitulo"] 
-    noticia["link"] = row["Link"]
-    liked.append(noticia)
-
-aux = {"news":news, "liked":liked}
-with open("/../Front-end/json-mock-api/src/db.json", "w") as f:
-	  json.dump (aux, f)
+if __name__ == '__main__':
+    main()
